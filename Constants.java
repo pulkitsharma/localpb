6605af6f06cf8d2bd24d8382fc01b7d354e3ae7a
pulkitsharma <pulkit.nitj@gmail.com>
2017-06-17 16:06:39 +0530

    Create Constants.java

---
+++ Constants.java
@@ -0,0 +1,18 @@
+package scripts;
+
+public class Constants {
+
+    public static String[] RARE_DROP_TABLE = {
+        "Uncut dragonstone", "Dragonstone", "Loop half of a key", "Tooth half of a key", "Crystal triskelion fragment 1",
+            "Crystal triskelion fragment 2", "Crystal triskelion fragment 3", "Dragon dagger", "Rune battleaxe", "Rune 2h sword",
+            "Dragon spear", "Rune sq shield", "Rune kiteshield", "Shield left half", "Dragon helm", "Onyx bolts", "Battlestaff",
+            "Vecna skull", "Water talisman", "Earth talisman", "Fire talisman", "Pure essence", "Nature rune", "Law rune", "Blood rune",
+            "Raw swordfish", "Raw shark", "Coal", "Adamantite ore", "Runite ore", "Adamant bar", "Rune bar", "Clean toadflax",
+            "Clean snapdragon", "Clean torstol", "Watermelon seed", "Magic seed", "Palm tree seed", "Torstol seed", "Yew logs",
+            "Brawling gloves (Melee)", "Brawling gloves (Magic)", "Brawling gloves (Ranged)", "Brawling gloves (Agility)", "Brawling gloves (Cooking)",
+            "Brawling gloves (FM)", "Brawling gloves (Fishing)", "Brawling gloves (Hunter)", "Brawling gloves (Mining)", "Brawling gloves (Prayer)",
+            "Brawling gloves (Smithing)", "Brawling gloves (Thieving)", "Brawling gloves (WC)",
+            // THESE ARE CUSTOM DROPS THAT AREN'T RDT BUT SHOULD BE LOOTED ANYWAYS
+            "Rune scimitar", "Gold charm", "Green charm", "Crimson charm", "Blue charm", "Grimy dwarf weed", "Grimy lantadyme", "Adamant arrow", "Rune arrow",
+            "Fire rune"
+    };
